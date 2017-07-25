fda36940136fd00fcc23b9eecef5141cfabda5b6
jianmyphy123 <jianmyphy123@tutanota.com>
2017-07-25 09:22:55 +0200

    state to props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,29 +15,57 @@ class App extends Component {
     this.props.addReminder(this.state.text);
   }
 
+  renderReminders() {
+    const { reminders } = this.props;
+    console.log('reminders', reminders);
+
+    return (
+      <ul className="list-group col-sm-4">
+        {
+          reminders.map(reminder => {
+            return (
+              <li key={reminder.id} className="list-group-item">
+                <div>{reminder.text}</div>
+              </li>
+            );
+          })
+        }
+      </ul>
+    );
+  }
+
   render() {
+
     return (
       <div className="App">
         <div className="title">
           Reminder Pro
         </div>
-        <div className="form-inline">
+        <div className="form-inline reminder-form">
           <div className="form-group">
             <input
-              className="form-contoll"
+              className="form-control"
               placeholder="I have to ..."
               onChange={event => this.setState({text: event.target.value})}/>
-            <button
-              type="button"
-              className="btn btn-success"
-              onClick={() => this.addReminder()}>
-              Add Reminder
-            </button>
           </div>
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() => this.addReminder()}>
+            Add Reminder
+          </button>
         </div>
+        { this.renderReminders() }
       </div>
     );
   }
 }
 
-export default connect(null, { addReminder })(App);
+function mapStateToProps( state) {
+  console.log(state);
+  return {
+    reminders: state
+  };
+}
+
+export default connect(mapStateToProps, { addReminder })(App);
